eacb5c03e4506d30666fb2ad370220b99e7536f1
PiyushMandaliya <60001807+PiyushMandaliya@users.noreply.github.com>
2021-11-29 20:26:25 -0500

    Initial Commit

---
+++ backend/src/main/java/com/waitingline/WaitinglineApplication.java
@@ -0,0 +1,13 @@
+package com.waitingline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitinglineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitinglineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/waitingline/WaitinglineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waitingline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitinglineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
