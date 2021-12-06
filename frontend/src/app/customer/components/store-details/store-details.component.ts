23a82c4264a10c86d353b5f36c711fdcf147060c
Nishantbatra360 <nishantbatra360@gmail.com>
2021-12-06 15:57:39 -0500

    queue details pop up added for web

--- frontend/src/app/customer/components/store-details/store-details.component.ts
+++ frontend/src/app/customer/components/store-details/store-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DialogRef } from '@progress/kendo-angular-dialog';
+import * as moment from 'moment';
 import { Store } from 'src/app/common/models/store.model';
 
 @Component({
@@ -24,5 +25,23 @@ export class CustomerStoreDetailsComponent implements OnInit {
   */
   closeForm(): void {
     this.dialog.close();
+  }
+
+  public get calc(){
+    var openingHourStr=this.store.openingHour.split(':');
+    var openingHourDate=new Date().setHours(parseInt(openingHourStr[0]),parseInt(openingHourStr[1]),parseInt(openingHourStr[2]));
+    var openingTime=moment(openingHourDate).format("HH:mm:ss");
+
+    var closingHourStr=this.store.closingHour.split(':');
+    var closingHourDate=new Date().setHours(parseInt(closingHourStr[0]),parseInt(closingHourStr[1]),parseInt(closingHourStr[2]));
+    var closingTime=moment(closingHourDate).format("HH:mm:ss");
+
+    var todayDate=new Date();
+    var todayTime=moment(todayDate).format("HH:mm:ss");
+
+    if(todayTime>=openingTime && todayTime<closingTime){
+      return "Store is open";
+    }
+    return "Store is closed";
   }
 }
