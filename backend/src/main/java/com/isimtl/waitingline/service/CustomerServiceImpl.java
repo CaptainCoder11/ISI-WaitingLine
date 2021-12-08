c0b60b36aa8672bfb51692676018e9778d94fc56
Nishantbatra360 <nishantbatra360@gmail.com>
2021-12-08 14:04:11 -0500

    backend fix

--- backend/src/main/java/com/isimtl/waitingline/service/CustomerServiceImpl.java
+++ backend/src/main/java/com/isimtl/waitingline/service/CustomerServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,7 +24,6 @@ public class CustomerServiceImpl implements ICustomerService {
         this.userRepository = userRepository;
         this.mailService = mailService;
     }
-
 
     @Override
     public List<User> findAll() {
