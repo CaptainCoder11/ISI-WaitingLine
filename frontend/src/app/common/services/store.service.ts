c26e5996077b34ad0e2922861abc3dce4973d0da
Ruchit Patel <ruchitpatel@Ruchits-MacBook-Air.local>
2022-01-03 14:35:48 -0500

    f

--- frontend/src/app/common/services/store.service.ts
+++ frontend/src/app/common/services/store.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Store } from '../models/store.model';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class StoreService {
